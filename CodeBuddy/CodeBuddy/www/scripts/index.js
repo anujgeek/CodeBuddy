0d1b8f256813f6a3c5f7063906885fc35636a827
anujgeek <anuj.geek@gmail.com>
2016-03-27 21:18:56 -0400

    Code

--- CodeBuddy/CodeBuddy/www/scripts/index.js
+++ CodeBuddy/CodeBuddy/www/scripts/index.js
@@ -1,28 +1,174 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
-// and then run "window.location.reload()" in the JavaScript Console.
-(function () {
-    "use strict";
+﻿var isDeviceReady = false;
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
+var SUCCESS = "Success";
+var FAILURE = "Failure";
+var EXCEPTION = "Exception";
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var element = document.getElementById("deviceready");
-        element.innerHTML = 'Device Ready';
-        element.className += ' ready';
+var app =
+    {
+        initialize: function ()
+        {
+            app.bindEvents();
+            app.uiinit();
+            app.applyKO();
+        },
+        bindEvents: function ()
+        {
+            document.addEventListener("deviceready", app.onDeviceReady, false);
+        },
+        onDeviceReady: function ()
+        {
+            isDeviceReady = true;
+            $(document).on("pagecontainerchange", function (event, ui)
+            {
+                if (ui.toPage[0].id == "page_recommendations_home")
+                {
+                    $("#page_recommendations_home_vsd").datebox('setTheDate', appViewModel.page_recommendations_home_vsd).trigger('datebox', { 'method': 'doset' });
+                    $("#page_recommendations_home_ved").datebox('setTheDate', appViewModel.page_recommendations_home_ved).trigger('datebox', { 'method': 'doset' });
+                }
+                if (ui.toPage[0].id == "page_recommendations_details")
+                {
+                    $("#page_recommendations_details_tab1").trigger("click");
+                }
+            });
+
+            appViewModel.username(window.sessionStorage.getItem("username"));
+
+            app.test();
+        },
+        uiinit: function ()
+        {
+            $(function ()
+            {
+                $("[data-role='footer']").html("<h4 class='ui-title' role='heading' aria-level='1'>&copy; " + new Date().getFullYear() + "</h4>");
+                $("body>[data-role='panel']").panel().enhanceWithin();
+            });
+        },
+        applyKO: function ()
+        {
+            ko.applyBindings(appViewModel);
+        },
+        test: function ()
+        {
+
+        },
+        openPanel: function ()
+        {
+            if (appViewModel.username() == null)
+            {
+                $("#leftpanel1").panel("open");
+            }
+            else
+            {
+                $("#leftpanel2").panel("open");
+            }
+        },
+        gotoNotification: function ()
+        {
+            //navigator.notification.alert("Travel Buddy - It is time for your next vacation", app.gotoNotificationDismissed, "Travel Buddy");
+            app.gotoNotificationDismissed();
+        },
+        gotoNotificationDismissed: function ()
+        {
+            app.testLoginInitialize();
+            app.gotoRecommendationsHome();
+        },
+        gotoLogin: function ()
+        {
+            $.mobile.changePage("#page_login", { transition: "slideup", changeHash: false });
+        },
+        gotoHome: function ()
+        {
+            $.mobile.changePage("#page_home", { transition: "slidedown", changeHash: false });
+        },
+        gotoRecommendationsHome: function ()
+        {
+            $.mobile.changePage("#page_recommendations_home", { transition: "slidefade", changeHash: false });
+        },
+        gotoRecommendationsHomeCustom: function ()
+        {
+            $.mobile.changePage("#page_recommendations_home_custom", { transition: "slidefade", changeHash: false });
+        },
+        gotoRecommendationsDetails: function ()
+        {
+            $.mobile.changePage("#page_recommendations_details", { transition: "slidefade", changeHash: false });
+        },
+        gotoPrivacyPolicy: function ()
+        {
+            $.mobile.changePage("#page_privacy_policy", { transition: "slidefade", changeHash: false });
+        },
+        gotoSettings: function ()
+        {
+            $.mobile.changePage("#page_settings", { transition: "slidefade", changeHash: false });
+        },
+        gotoRecommendationsTravel: function ()
+        {
+            $.mobile.changePage("#page_recommendations_travel", { transition: "slidefade", changeHash: false });
+        },
+        gotoRecommendationsFood: function ()
+        {
+            $.mobile.changePage("#page_recommendations_food", { transition: "slidefade", changeHash: false });
+        },
+        testLoginInitialize: function ()
+        {
+            appViewModel.username("TestUser");
+            window.sessionStorage.setItem("username", appViewModel.username());
+
+            appViewModel.place("South India");
+            appViewModel.placeLink("https://en.wikipedia.org/wiki/South_India");
+            appViewModel.page_recommendations_home_vsd = new Date("Jan 1, 2016");
+            appViewModel.page_recommendations_home_ved = new Date("Jan 3, 2016");
+        },
+        testLoginSuccess: function ()
+        {
+            app.testLoginInitialize();
+
+            app.gotoHome();
+        },
+        testLogout: function ()
+        {
+            appViewModel.username(null);
+            window.sessionStorage.setItem("username", null);
+            $.mobile.changePage("#page_login", { transition: "slideup", changeHash: false });
+        },
+        loginSuccess: function (x)
+        {
+            FB.api('/me', function (response)
+            {
+                appViewModel.username(response.name);
+                window.sessionStorage.setItem("username", appViewModel.username());
+
+                appViewModel.place("South India");
+                appViewModel.placeLink("https://en.wikipedia.org/wiki/South_India");
+                appViewModel.page_recommendations_home_vsd = new Date("Jan 1, 2016");
+                appViewModel.page_recommendations_home_ved = new Date("Jan 3, 2016");
+
+                app.gotoHome();
+            });
+        },
+        loginFailure: function (x)
+        {
+            navigator.notification.alert("Please enter valid credentials!", null, "Login Failure");
+        },
+        logout: function ()
+        {
+            FB.logout(function (response)
+            {
+                appViewModel.username(null);
+                window.sessionStorage.setItem("username", null);
+                $.mobile.changePage("#page_login", { transition: "slideup", changeHash: false });
+            });
+        }
     };
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
+var appViewModel =
+    {
+        username: ko.observable(null),
+        place: ko.observable(null),
+        placeLink: ko.observable(null),
+        page_recommendations_home_vsd: ko.observable(null),
+        page_recommendations_home_ved: ko.observable(null),
+        page_recommendations_home_custom_location: ko.observable(null),
+        page_recommendations_home_custom_vsd: ko.observable(null),
+        page_recommendations_home_custom_ved: ko.observable(null)
     };
-
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
